f07300a839573b53f28b8ceb0a005b3c8df15313
fierzahaikkal <ojogames.id@gmail.com>
2022-08-20 14:39:01 +0700

    box-wrap finish commit

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ function toggleMenu(){
   navigation.classList.toggle('active');
   menuToggle.classList.toggle('active');
 }
+
+let valuesWrap = document.querySelectorAll(".num");
+let interval = 5000;
+
+
+valuesWrap.forEach((valDisplay) => {
+  let start = 0;
+  let end = parseInt(valDisplay.getAttribute("data-val"));
+  let time = Math.floor(interval / end);
+  let count = setInterval(function (){
+    start += 1;
+    valDisplay.textContent = start;
+    if(start === end){
+      clearInterval(count);
+    }
+  }, time);
+});
